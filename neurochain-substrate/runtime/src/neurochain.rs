e692e2f8a93717d4312034b0b755339e7441c05c
pavelkrolevets <pavelkrolevets@gmail.com>
2019-12-18 19:15:35 +0800

    modified:   runtime/src/neurochain.rs

--- neurochain-substrate/runtime/src/neurochain.rs
+++ neurochain-substrate/runtime/src/neurochain.rs
@@ -8,9 +8,10 @@ use parity_codec::{Encode, Decode};
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct NN_Model<Hash, Balance> {
     id: Hash,
-    Weights: u64,
-    Intercept: u64,
-    LearningRate: u64,
+    Weights: i64,
+    Intercept: i64,
+    LearningRate: i64,
+    Loss: i64,
     Bounty: Balance,
 }
 
@@ -29,8 +30,8 @@ decl_storage! {
         // ParticipantsIndex: map T::Hash => u64;
 
         Nonce: u64;
-        ToFloat get(to_float) : u64 = 1_000_000_000;
-        Prediction get(get_prediction): u64;
+        ToFloat get(to_float) : i64 = 1_000_000_000;
+        Prediction get(get_prediction): i64;
     }
 }
 
@@ -38,7 +39,7 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event<T>() = default;
 
-        fn create_model(origin, weight: u64, intercept: u64, learnRate: u64) -> Result {
+        fn create_model(origin, weight: i64, intercept: i64, learnRate: i64) -> Result {
             let sender = ensure_signed(origin)?;
             let nonce = <Nonce<T>>::get();
             let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
@@ -51,6 +52,7 @@ decl_module! {
                 Weights: weight,
                 Intercept: intercept,
                 LearningRate: learnRate,
+                Loss: 0 as i64,
                 Bounty: <T::Balance as As<u64>>::sa(0),
             };
 
@@ -59,7 +61,7 @@ decl_module! {
 
             Ok(())
         }
-        fn update_model(origin, model_id: T::Hash, new_weight: u64, new_intercept: u64, new_learnRate: u64) -> Result {
+        fn update_model(origin, model_id: T::Hash, new_weight: i64, new_intercept: i64, new_learnRate: i64) -> Result {
             let sender = ensure_signed(origin)?;
             ensure!(<Models<T>>::exists(model_id), "This model doesnt exit");
             //Get model
@@ -90,52 +92,72 @@ decl_module! {
             Ok(())
         }
 
-        fn train_model(origin, model_id: T::Hash, data: u64, classification: u64) -> Result {
+        fn train_model(origin, model_id: T::Hash, data: i64, classification: i64) -> Result {
             let sender = ensure_signed(origin)?;
             ensure!(<Models<T>>::exists(model_id), "This model doesnt exit");
             //Get model
             let mut model = Self::stored_model(&model_id);
             let to_float = Self::to_float();
-            let mut prediction = model.Intercept;
-            let mut _update = (data * model.LearningRate / to_float) as u64;
-            let mut new_weights: u64 = 0;
+            let mut prediction = data * model.Weights + model.Intercept;
+            let mut _update = (data * model.LearningRate / to_float) as i64;
+            let mut new_weights: i64 = 0;
+            let mut total_loss =  (classification - prediction).pow(2)/2 as i64;
+            let mut _norm: i64 = 0;
+            // Compute gradients
+            let  d_total_loss_d_out = - (prediction - classification); 
+            let  d_out_d_w = data;
+            let  d_total_loss_d_w = d_total_loss_d_out * d_out_d_w; //using chain rule
+            
             // Update weights
-            let mut _norm: u64 = 0;
-            if classification > 0 {
-                prediction = data * model.Weights + model.Intercept;
-                new_weights = model.Weights + _update;
-                _norm = _norm + data * data;
-            } else {
-                // sign -1
-                prediction = data * model.Weights + model.Intercept;
-                new_weights = model.Weights - _update;
-                _norm = _norm + data * data;
-            }
+            new_weights = new_weights - model.LearningRate * d_total_loss_d_w;
 
-            if prediction <= 0{
-                prediction = 0;
-            } else {
-                prediction = 1;
-            }
+            //Compute new loss
+            let new_loss = (classification - (data * new_weights + model.Intercept)).pow(2)/2 as i64;
+
+            // Commit new model
+
+            model.Weights = new_weights;
+            model.Loss = new_weights;
+            <Models<T>>::insert(&model_id, model);
+            
+            // if classification > 0 {
+            //     prediction = data * model.Weights + model.Intercept;
+            //     new_weights = model.Weights + _update;
+            //     _norm = _norm + data * data;
+            // } else {
+            //     // sign -1
+            //     prediction = data * model.Weights + model.Intercept;
+            //     new_weights = model.Weights - _update;
+            //     _norm = _norm + data * data;
+            // }
+
+            // if prediction <= 0{
+            //     prediction = 0;
+            // } else {
+            //     prediction = 1;
+            // }
+            
             // Must be almost within `toFloat` of `toFloat*toFloat` because we only care about the first `toFloat` digits.
-            let mut oneSquared = to_float * to_float;
-            let offset = to_float * 100;
-            ensure!(oneSquared - offset < _norm && _norm < oneSquared + offset, "The provided data does not have a norm of 1.");
 
-            if prediction != classification {
-                model.Weights = new_weights;
-                <Models<T>>::insert(&model_id, model);
-            }
+            // let mut oneSquared = to_float * to_float;
+            // let offset = to_float * 100;
+            // ensure!(oneSquared - offset < _norm && _norm < oneSquared + offset, "The provided data does not have a norm of 1.");
+
+            // if prediction != classification {
+            //     model.Weights = new_weights;
+            //     <Models<T>>::insert(&model_id, model);
+            // }
+
             Ok(())
         }
 
-        fn predict (origin, model_id: T::Hash, data: u64) -> Result {
+        fn predict (origin, model_id: T::Hash, data: i64) -> Result {
             let sender = ensure_signed(origin)?;
             ensure!(<Models<T>>::exists(model_id), "This model doesnt exit");
             //Get model
             let mut model = Self::stored_model(&model_id);
             let m = model.Weights * data + model.Intercept;
-            
+
             <Prediction<T>>::put(m);
             Ok(())
         }
